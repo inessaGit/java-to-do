5de9fdaef0d9d95e1fc938c42c497a0a960b0de3
Courtney Phillips <courtney@epicodus.com>
2016-09-16 15:38:38 -0700

    Import sql2o in CategoryTest class, define @Before and @After blocks to handle connecting to the database, and clearing out test database.

--- src/test/java/CategoryTest.java
+++ src/test/java/CategoryTest.java
@@ -1,7 +1,23 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import org.sql2o.*;
 
 public class CategoryTest {
+
+  @Before
+  public void setUp() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/to_do_test", null, null);
+  }
+
+  @After
+  public void tearDown() {
+    try(Connection con = DB.sql2o.open()) {
+      String deleteTasksQuery = "DELETE FROM tasks *;";
+      String deleteCategoriesQuery = "DELETE FROM categories *;";
+      con.createQuery(deleteTasksQuery).executeUpdate();
+      con.createQuery(deleteCategoriesQuery).executeUpdate();
+    }
+  }
 
   @Test
   public void category_instantiatesCorrectly_true() {
