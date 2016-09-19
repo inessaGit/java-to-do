2c25eb1de54484f0d275e1f5476083fc80e9f070
Courtney Phillips <courtney@epicodus.com>
2016-09-19 13:31:48 -0700

    Define route to handle deleting Task objects.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -37,6 +37,16 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    post("/categories/:category_id/tasks/:id/delete", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Task task = Task.find(Integer.parseInt(request.params("id")));
+      Category category = Category.find(task.getCategoryId());
+      task.delete();
+      model.put("category", category);
+      model.put("template", "templates/category.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
     post("/tasks", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       Category category = Category.find(Integer.parseInt(request.queryParams("categoryId")));
