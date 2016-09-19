3f5c6411e3b56ff68e77d9a62a174cfa7e1a860e
Courtney Phillips <courtney@epicodus.com>
2016-09-19 10:36:23 -0700

    Move Task and Category forms to index.vtl template.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -23,13 +23,6 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    get("/tasks", (request, response) -> {
-      Map<String, Object> model = new HashMap<String, Object>();
-      model.put("tasks", Task.all());
-      model.put("template", "templates/tasks.vtl");
-      return new ModelAndView(model, layout);
-    }, new VelocityTemplateEngine());
-
     post("/tasks", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       Category category = Category.find(Integer.parseInt(request.queryParams("categoryId")));
@@ -38,14 +31,6 @@ public class App {
       newTask.save();
       model.put("category", category);
       model.put("template", "templates/category-task-success.vtl");
-      return new ModelAndView(model, layout);
-    }, new VelocityTemplateEngine());
-
-    get("/tasks/:id", (request, response) -> {
-      Map<String, Object> model = new HashMap<String, Object>();
-      Task task = Task.find(Integer.parseInt(request.params(":id")));
-      model.put("task", task);
-      model.put("template", "templates/task.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
