186062219af9c7729f2f8ef8ae9db2c7b17a42f1
Courtney Phillips <courtney@epicodus.com>
2016-09-19 13:29:23 -0700

    Define delete method in Task class to remove individual Task objects from SQL database.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -86,5 +86,14 @@ public class Task {
       .executeUpdate();
     }
   }
-  
+
+  public void delete() {
+    try(Connection con = DB.sql2o.open()) {
+    String sql = "DELETE FROM tasks WHERE id = :id;";
+    con.createQuery(sql)
+      .addParameter("id", id)
+      .executeUpdate();
+    }
+  }
+
 }
