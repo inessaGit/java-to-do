af1eba2738e4daba132383ddb9bc174320c86ca5
inessa <inessa@192.168.2.105>
2018-06-11 10:05:19 +0200

    6/11

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,6 @@
 import java.util.Map;
 import java.util.HashMap;
 import spark.ModelAndView;
-import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
